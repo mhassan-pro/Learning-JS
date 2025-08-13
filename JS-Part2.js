4f8c2e95325c92ff4ceff4fdc0df58020215009e
mhassan-pro <mhassan.pro0@gmail.com>
2025-08-13 20:12:20 +0500

    Add golden string check in JS-Part2.js to determine if a string starts with 'A' or 'a' and has a length greater than 5

--- JS-Part2.js
+++ JS-Part2.js
@@ -56,3 +56,18 @@ switch (quarter)
 }
 
 console.log("---------------------------------------------------------------------");
+
+/* Qs4. A string is a golden string if it starts with the character ‘A’ or ‘a’ and has a total
+length greater than 5.
+For a given string print if it is golden or not. */
+
+let str = "Asphalt";
+
+if ((str[0]=== 'A' || str[0] === 'a') && str.length > 5)
+{
+    console.log("It is golden");
+}
+else 
+{
+    console.log("It is not golden");
+}
