f23f4ed412eb51f9cc9d711f232db835b87f5178
mhassan-pro <mhassan.pro0@gmail.com>
2025-08-13 19:13:44 +0500

    Add switch statement in JS-Part2.js to print months by quarter and added console logs for better output separation

--- JS-Part2.js
+++ JS-Part2.js
@@ -10,6 +10,7 @@ else
 {
     console.log("Bad")
 }
+console.log("---------------------------------------------------------------------");
 
 /* Qs2. Take the user's name & age as input using prompts.
 Then return back the following statement to the user as an alert (by substituting
@@ -22,3 +23,36 @@ let userAge = prompt("Enter user's age");
 
 let massage = `${userName} is ${userAge} year's old.`;
 alert(massage);
+
+console.log("---------------------------------------------------------------------");
+/* Qs3. Write a switch statement to print the months in a quarter.
+Months in Quarter 1 : January, February, March
+Months in Quarter 2 : April, May, June
+Months in Quarter 3 : July, August, September
+Months in Quarter 4: October, November, December.
+[Use the number as the case value in switch]
+ */
+let quarter = 3;
+switch (quarter)
+{
+    case 1:
+        console.log("January, February, March");
+        break;
+
+        case 2:
+        console.log("April, May, June");
+        break;
+
+        case 3:
+        console.log("July, August, September");
+        break;
+
+        case 4:
+        console.log("October, November, December.");
+        break;
+
+        default:
+        console.log("Invalid quarter number!");   
+}
+
+console.log("---------------------------------------------------------------------");
