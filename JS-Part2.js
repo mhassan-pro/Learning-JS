10e3006b71fdc16d30d2831b5b2d29348e7e75e9
mhassan-pro <mhassan.pro0@gmail.com>
2025-08-13 18:39:46 +0500

    Add JS-Part2.js: Implement number divisibility check and user input prompts

---
+++ JS-Part2.js
@@ -0,0 +1,18 @@
+/* Qs1. Create a number variable num with some value.
+Now, print “good” if the number is divisible by 10 and print “bad” if it is not. */
+
+let num = 10;
+if (num % 10 == 0)
+{
+    console.log("Good");
+}
+else
+{
+    console.log("Bad")
+}
+
+/* Qs2. Take the user's name & age as input using prompts.
+Then return back the following statement to the user as an alert (by substituting
+their name & age) :
+name is age years old.
+[Use template Literals to print this sentence] */ 
