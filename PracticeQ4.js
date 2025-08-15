46dbd01cc37eee0d7c941686910540cd125b5be9
mhassan-pro <mhassan.pro0@gmail.com>
2025-08-15 16:55:32 +0500

    Add PracticeQ4.js: Implement string trimming and uppercase conversion with console output

---
+++ PracticeQ4.js
@@ -0,0 +1,9 @@
+/* for the given string 
+let msg = "help!v          ";
+Trim and convert it to uppercase */
+
+let msg = "Help!               ";
+let trimAndUpper = msg.trim().toUpperCase();
+console.log(trimAndUpper);
+
+/*  */
