7f1c58fbe4fa5528f8bb3a4c3bfe9f2867fa2d14
mhassan-pro <mhassan.pro0@gmail.com>
2025-08-15 17:06:15 +0500

    Update PracticeQ4.js: Refine string manipulation examples and add new console outputs for slicing and replacing substrings

--- PracticeQ4.js
+++ PracticeQ4.js
@@ -1,9 +1,13 @@
 /* for the given string 
-let msg = "help!v          ";
+let msg = "help!          ";
 Trim and convert it to uppercase */
 
 let msg = "Help!               ";
 let trimAndUpper = msg.trim().toUpperCase();
 console.log(trimAndUpper);
 
-/*  */
+/* For the string -> let name = "Apna College", predict the output of the following .  */
+let name = "ApnaCollege";
+console.log(name.slice(4 , 9)); //Colle
+console.log(name.indexOf("na")); //2
+console.log(name.replace("Apna","Our")); //OurCollege
