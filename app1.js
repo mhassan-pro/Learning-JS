d51690bc836bbcaeeaf1b359c54efac868063007
mhassan-pro <mhassan.pro0@gmail.com>
2025-08-17 16:17:50 +0500

    Add app1.js: Introduce loops with examples for printing numbers and odd numbers from 1 to 15

---
+++ app1.js
@@ -0,0 +1,23 @@
+// Loops ( use to iterate a piece of code).
+
+// for loop syntax [ for (initinalization; condition; updation) { statements } ].
+
+// practice Q print 1 to 5 numbers.
+
+for ( let i = 1 ; i<=5 ; i++){
+    console.log(i);
+}
+
+console.log("----------------------------");
+
+// print odd numbers from 1 to 15.
+
+for ( let j = 1; j<=15; j++)
+{
+    if ( !(j % 2 == 0) )
+    {
+        console.log(j);
+    }
+}
+
+console.log("----------------------------");
