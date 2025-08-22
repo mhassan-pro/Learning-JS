20e1ccb8acdd96e5f100d1733efa3394955f50a6
mhassan-pro <mhassan.pro0@gmail.com>
2025-08-22 17:37:02 +0500

    Implement factorial calculation in PracticeQ9.js

--- PracticeQ9.js
+++ PracticeQ9.js
@@ -38,3 +38,23 @@ while(copy1 > 0 )
     copy1 = Math.floor(copy1/10);
 }
 console.log(sum);
+
+/* Qs4. Print the factorial of a number n.
+[Factorial of a number n is the product of all positive integers less than or equal to a
+given positive integer and denoted by that integer. ]
+Example :
+7! (factorial of 7) = 1x2x3x4x5x6x7 = 5040
+5! (factorial of 5) = 1x2x3x4x5 = 120
+3! (factorial of 3) = 1x2x3 = 6
+0! Is always 1
+ */
+
+let  n = 7;
+let fact = 1;
+
+for (let i = 1; i<=n ; i++)
+{
+    fact =fact * i;
+
+}
+console.log(fact);
