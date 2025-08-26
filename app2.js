e60831c93269ce495da8bb800fee39667bda8fcb
mhassan-pro <mhassan.pro0@gmail.com>
2025-08-26 15:26:59 +0500

    Add object literal for thread/post properties and update console log for student

--- app2.js
+++ app2.js
@@ -7,6 +7,25 @@ let student = {
     name:"Hassan",
     age: 23,
     marks: 80
-};
 
-console.log(student);
+};
+console.log(student.marks);
+
+// Thread / Twitter Post
+/* Creating an object literal for the
+ properties of thread / twitter post includes 
+ -> username
+ -> content
+ -> likes
+ -> reports
+ -> tags */
+
+ const post = {
+    username : "@muhammadhassan",
+    content : "Develpor Road-Map",
+    likes : 200,
+    reports : 5,
+    tags : ["@apnacollege", "@developerroadmap"]
+
+ };
+
