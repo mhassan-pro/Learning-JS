2021d3fe40f8b792f7e1d42ad51bb40e2b56e292
mhassan-pro <mhassan.pro0@gmail.com>
2025-08-27 19:12:32 +0500

    Add random number generation examples in PracticeQ10.js and app2.js

---
+++ PracticeQ10.js
@@ -0,0 +1,9 @@
+// Generate random number beteem 1 to 100
+
+let randomNumber = Math.floor(Math.random() * 100) +1;
+
+console.log("Random number between 1 to 100 is : ", randomNumber);
+
+// generate a random number between 1 to 5.
+let newRandomNumber = Math.floor(Math.random() * 5) +1;
+console.log("Random number between 1 to 5 is : ",newRandomNumber);
