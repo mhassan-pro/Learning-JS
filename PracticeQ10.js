106fc830ad935561153673b08539a190d2257aef
mhassan-pro <mhassan.pro0@gmail.com>
2025-08-27 20:49:50 +0500

    Fix random number generation logic in PracticeQ10.js and update variable in app2.js

--- PracticeQ10.js
+++ PracticeQ10.js
@@ -7,3 +7,22 @@ console.log("Random number between 1 to 100 is : ", randomNumber);
 // generate a random number between 1 to 5.
 let newRandomNumber = Math.floor(Math.random() * 5) +1;
 console.log("Random number between 1 to 5 is : ",newRandomNumber);
+
+// Guessing Game
+const max = prompt("Enter the max number");
+
+const  random = Math.floor(Math.random() * n) + 1;
+
+let guess = prompt("Guess the random number");
+
+while(true){
+    if( guess === ("quit" || "Quit"))
+    {
+        console.log("You Quit ");
+        break;
+    }
+    if (guess == random)
+    {
+        console.log("You win")
+    }
+};
