a31f4e15bb316058e7e8a4f287155abeed0dfef8
mhassan-pro <mhassan.pro0@gmail.com>
2025-08-29 21:51:41 +0500

    Add unique character extraction and longest country name functions in JS-Part6.js

--- JS-Part6.js
+++ JS-Part6.js
@@ -10,8 +10,37 @@ function largerThan (arr,num){
         }
     }
 }
-console.log(largerThan(arr,num));
+ largerThan(arr,num);
 
 console.log("------------------------------------------");
 
+/* Qs2. Write a JavaScript function to extract unique characters from a string.
+Example: str = “abcdabcdefgggh”
+ans = “abcdefgh”
+ */
+let str = "abcdabcdefgggh";
+function uniqueChar(str){
+    
+    console.log(str.slice());
+}
+uniqueChar(str);
 
+/* Qs3. Write a JavaScript function that accepts a list of country names as input and
+returns the longest country name as output.
+Example : country = ["Australia", "Germany", "United States of America"]
+output : "United States of America"
+ */
+ let country = ["Australia", "Germany", "United States of America"];
+ function longestCountry (country){
+    for(let i = 0; i < country.length ; i++){
+
+    }
+ }
+
+ /* Qs5. Write a JavaScript function to generate a random number within a range 
+ (start,end).  */
+
+ function randomNumber(start,end){
+    let random = Math.floor(Math.random() *end) + start;
+    console.log(random);
+ }
