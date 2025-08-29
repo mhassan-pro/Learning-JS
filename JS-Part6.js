f31fed6bab6205a120f4d25c22f768add70e0734
mhassan-pro <mhassan.pro0@gmail.com>
2025-08-29 19:52:23 +0500

    Implement function to log array elements larger than a specified number in JS-Part6.js

--- JS-Part6.js
+++ JS-Part6.js
@@ -1,4 +1,17 @@
 //Qs1. Write a JavaScript function that returns array elements larger than a number.
 
 let arr = [1,3,5,7,12,11,10,9];
+let num = 4;
+
+function largerThan (arr,num){
+    for(let i = 0; i<arr.length; i++){
+        if(arr[i] > num){
+            console.log(arr[i]);
+        }
+    }
+}
+console.log(largerThan(arr,num));
+
+console.log("------------------------------------------");
+
 
