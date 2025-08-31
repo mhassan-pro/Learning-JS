cbc5a4c18dc43eb6d22e11018d2d459a647cc079
mhassan-pro <mhassan.pro0@gmail.com>
2025-08-31 19:22:22 +0500

    Fix formatting and add missing newlines in PracticeQ12.js

--- PracticeQ12.js
+++ PracticeQ12.js
@@ -2,4 +2,15 @@
 
 const square = (n) => (
      n**2 );
-console.log( square(6) )
+console.log( square(6) );
+
+// write a function that prints "Hello World" 5 times at interval of 2 sec.
+
+
+let id = setInterval(() =>{
+    console.log("Hello World");
+},2000);
+
+setTimeout(() => {
+    clearInterval(id);
+},10000);
