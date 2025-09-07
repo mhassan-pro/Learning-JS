0c549ae1f9a03996a0490114885302018e788389
mhassan-pro <mhassan.pro0@gmail.com>
2025-09-07 15:30:22 +0500

    Add implementation for creating a new array with elements incremented by 5 using map function

--- JS-Part8.js
+++ JS-Part8.js
@@ -13,4 +13,10 @@ console.log(sum);
 let avg = sum / nums.length;
 console.log(avg);
 
-/*  */
+/* Qs2.Create a new array using the map function
+ whose each elementis equal to the original element plus 5. */
+
+let arr = [1,2,3,4,5];
+let newArr = arr.map( (el) => el + 5 );
+
+console.log(newArr); 
