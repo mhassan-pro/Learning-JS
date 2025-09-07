91b1198d5df379890f17e78b0bcf8e821e04d93b
mhassan-pro <mhassan.pro0@gmail.com>
2025-09-07 22:08:00 +0500

    Add isEven function to determine if a number is even and log output

--- JS-Part7.js
+++ JS-Part7.js
@@ -7,3 +7,12 @@ let arrayAverage = (arr) => {
 };
 
 console.log("Output :",arrayAverage([2,4,6,8,3,4,6,5]));
+console.log("==========================================");
+
+/* Qs2. Write an arrow function named isEven() that takes a single number as argument
+and returns if it is even or not. */
+let isEven = (num) => {
+    let reminder = num.every((el) => el % 2)
+    return reminder == 0;
+}
+console.log("Output :",isEven([2]));
