bb52bfc5e174d802ca025379f97e05c34c87a4cc
mhassan-pro <mhassan.pro0@gmail.com>
2025-09-07 16:14:28 +0500

    Refactor console.log statements to include labels for output clarity

--- JS-Part8.js
+++ JS-Part8.js
@@ -20,7 +20,7 @@ console.log("==============================");
 let arr = [1,2,3,4,5];
 let newArr = arr.map( (el) => el + 5 );
 
-console.log(newArr); 
+console.log("Output :",newArr); 
 console.log("==============================");
 
 /* Qs3. Create a new array whose elements are in uppercase of words present in the
@@ -29,7 +29,7 @@ original array.
 
 let words = ["hello","hi","bye","good","bad","new"];
 let upperCaseWords = words.map( (el) => el.toUpperCase() );
-console.log(upperCaseWords);
+console.log("Output :",upperCaseWords);
 
 console.log("==============================");
 /* Qs4. Write a function called doubleAndReturnArgs which accepts an array and a
@@ -41,8 +41,14 @@ let doubleAndReturnArgs = (arr,...args) =>
     ...arr,...args.map((el) => el * 2),
 ];
 
-console.log( doubleAndReturnArgs([2,6,4],223,55)
+console.log("Output :", doubleAndReturnArgs([2,6,4],223,55)
 )
 
 
 console.log("==============================");
+
+/* Qs5. Write a function called mergeObjects that accepts two objects and returns a new
+object which contains all the keys and values of the first object and second object. */
+
+let mergeObjects = (obj1,obj2) => ( {...obj1,...obj2} );
+console.log("Output :",mergeObjects({name:"Hassan",age23},{day:"Sunday",date:"7 Sept 2024"}));
