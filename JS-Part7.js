32897e3726b2e0df692a94940822be6e4a510d06
mhassan-pro <mhassan.pro0@gmail.com>
2025-09-07 21:57:31 +0500

    Implement arrayAverage function to calculate the average of an array of numbers

--- JS-Part7.js
+++ JS-Part7.js
@@ -1,2 +1,9 @@
-const arrayAverage = (arr) => {};
-let arr = [80,97,83];
+/* Qs1. Write an arrow function named arrayAverage that accepts an array of numbers
+and returns the average of those numbers. */
+
+let arrayAverage = (arr) => {
+     let sum = arr.reduce( (sum,el) => sum + el);
+    return sum / arr.length;
+};
+
+console.log("Output :",arrayAverage([2,4,6,8,3,4,6,5]));
