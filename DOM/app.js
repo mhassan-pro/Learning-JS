a3a6be443da0669699a7ed33fbc14046ff19530e
mhassan-pro <mhassan.pro0@gmail.com>
2025-09-10 18:53:38 +0500

    Add additional console.dir statements for improved debugging of DOM elements

--- DOM/app.js
+++ DOM/app.js
@@ -10,3 +10,9 @@ console.dir(document.querySelector("h1") );
 console.dir(document.querySelector("#discription") );
 
 console.dir(document.querySelector(".oldImg") );
+
+console.dir(document.querySelector("div a") );
+
+console.dir(document.querySelectorAll("p") );
+
+console.dir(document.querySelectorAll("div a") );
