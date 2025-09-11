2f495061afd061239c6e4883ef2b0c4352eec8fd
mhassan-pro <mhassan.pro0@gmail.com>
2025-09-11 14:04:15 +0500

    Add button styling and apply class to change appearance

--- JS-Part9.js
+++ JS-Part9.js
@@ -17,3 +17,8 @@ buttonElement.innerText = "Click me";
 buttonElement.setAttribute('id','btn');
 inputElement.setAttribute('type', 'text')
 
+/* Qs3. Access the btn using the querySelector and button id. Change the button
+background color to blue and text color to white.
+ */
+let btn = document.querySelector('#btn');
+btn.classList.add('btnStyle');
