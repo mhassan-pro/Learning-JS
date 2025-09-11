05493e92a57109d6728b3d6bbcf43b154ade70e8
mhassan-pro <mhassan.pro0@gmail.com>
2025-09-11 13:03:07 +0500

    Replace old image manipulation code with new paragraph creation and appending; add red text paragraph to the body.

--- DOM/app.js
+++ DOM/app.js
@@ -1,18 +1,5 @@
-let smallImages = document.getElementsByClassName("oldImg");
- 
-for( let i=0; i<smallImages.length; i++){
-    smallImages[i].src = "assets/spiderman_img.png";
-    console.log(`Value of imges no. ${i} is changed`);
-}
-
-console.dir(document.querySelector("h1") );
-
-console.dir(document.querySelector("#discription") );
-
-console.dir(document.querySelector(".oldImg") );
-
-console.dir(document.querySelector("div a") );
-
-console.dir(document.querySelectorAll("p") );
-
-console.dir(document.querySelectorAll("div a") );
+let newPara = document.createElement('p');
+newPara.innerText = "Hey I'm red!";
+newPara.style.color = "red";
+let body = document.querySelector('body');
+body.appendChild(newPara);
