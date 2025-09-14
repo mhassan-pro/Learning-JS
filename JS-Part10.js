4549107c8622c96c4d26ddaf7230b11635cab847
mhassan-pro <mhassan.pro0@gmail.com>
2025-09-14 23:56:26 +0500

    Reorder input and heading elements in HTML structure; update JavaScript to reflect changes in event handling

--- JS-Part10.js
+++ JS-Part10.js
@@ -6,3 +6,11 @@ btn.innerText = "Click !me"
 btn.addEventListener('click' , function(){
     btn.style.backgroundColor = "blue"; 
 })
+
+let input = document.querySelector('input');
+let h2 = document.querySelector('h2');
+
+input.addEventListener('input', function(){
+    h2.innerText = input.value;
+})
+
