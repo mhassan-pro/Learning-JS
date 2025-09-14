57243397bebb1b22683e67d2fe995968c12ce00e
mhassan-pro <mhassan.pro0@gmail.com>
2025-09-14 21:11:07 +0500

    Add CSS styles for body, button, and div elements; update JavaScript to apply classes and inner text

--- PracticeQ15.js
+++ PracticeQ15.js
@@ -6,7 +6,10 @@ heading.innerText = "Generate a random color";
 let btn = document.createElement('button');
 body.append(btn);
 btn.innerText = "Generate Color";
+btn.classList.add('btn')
 
 let box = document.createElement('div');
 body.append(box);
+box.innerText = "This is your new color";
+box.classList.add('div-box');
 
