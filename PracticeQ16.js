6b2ccc9de37c51340865a5bc7065135666c1413e
mhassan-pro <mhassan.pro0@gmail.com>
2025-09-15 14:52:15 +0500

    Add delete functionality for list items in To-Do App

--- PracticeQ16.js
+++ PracticeQ16.js
@@ -7,4 +7,20 @@ btn.addEventListener('click', function() {
     item.innerText = input.value;
     input.value = "";
     ul.appendChild(item);
-})
+
+
+    let delBtn = document.createElement('button');
+    delBtn.innerText = "Delete";
+    delBtn.classList.add('delete')
+    item.appendChild(delBtn)
+
+    let delBtns = document.querySelectorAll(".delete");
+    for(delBtn of delBtns){
+    delBtn.addEventListener('click' , function(){
+    let par = this.parentElement;
+    par.remove();
+    });
+     }
+
+});
+
