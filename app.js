de5d04f717eb77b29047d54b86284d46bca681c3
mhassan-pro <mhassan.pro0@gmail.com>
2025-09-18 22:17:19 +0500

    Remove commented-out color change function and related code for clarity

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
-let h1 = document.querySelector("h1");
+// let h1 = document.querySelector("h1");
 
-function changeColor(color, delay, nextColorChange){
-    setTimeout(() => {
-        h1.style.color = color;
-       if( nextColorChange ) nextColorChange();
-    },delay)
+// function changeColor(color, delay, nextColorChange){
+//     setTimeout(() => {
+//         h1.style.color = color;
+//        if( nextColorChange ) nextColorChange();
+//     },delay)
 
+// }
+// changeColor("red",1000,() => {
+//     changeColor("orange",1000,()=>{
+//         changeColor("blue",1000,() => {
+//             changeColor("green",1000,() =>{
+//                 changeColor("purple",1000,() =>{});
+//             });
+//         });
+//     });
+// });
+
+
+function saveToDb(data, success, failure){
+    let randomInternetSpeed = Math.floor(Math.random() *10) + 1;
+    if(randomInternetSpeed > 5){
+        success();
+    } else{
+       failure();
+    }
 }
-changeColor("red",1000,() => {
-    changeColor("orange",1000,()=>{
-        changeColor("blue",1000,() => {
-            changeColor("green",1000,() =>{})
-        })
-    })
+
+saveToDb("apna college",() => {
+     console.log("your data was saved");
+}, () => {
+     console.log("weak connection data was not saved")
 });
