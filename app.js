155e2ba84802d795ff7568c68ef3b7c8863a74b9
mhassan-pro <mhassan.pro0@gmail.com>
2025-09-19 23:09:50 +0500

    Refactor changeColor and demo functions for improved asynchronous color handling; enhance getNum function logging

--- app.js
+++ app.js
@@ -16,17 +16,41 @@ let h1 = document.querySelector("h1");
 //     return 5;
 // }
 
+ function changeColor(color, delay){
+   return new Promise((resolve,reject) => {
+         setTimeout(() => {
+        h1.style.color = color;
+        console.log("Color changed to ",color)
+        resolve("color changed")
+    },delay)
+
+    })
+}
+
+async function demo(){
+    await changeColor("pink",1000);
+    await changeColor("red",1000);
+    await changeColor("green",1000);
+    await changeColor("blue",1000);
+    await changeColor("yellow",1000);
+    await changeColor("orange",1000);
+}
+
+
+
 function getNum (){
     return new Promise((resolved,reject) => {
         setTimeout(() => {
             let num = Math.floor(Math.random() * 10) + 1;
-            console.log(num);
-            resolved()
+            console.log(num)
+            resolved();
         },1000);
 
     })
 }
 
 async function greet(){
-    getNum();
+     for(let i = 0;i< 10 ;i++){
+        await getNum();
+     }
 }
