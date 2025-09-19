360ded1d3adf0c8bb591dd1745fd799692f9cd67
mhassan-pro <mhassan.pro0@gmail.com>
2025-09-19 23:15:39 +0500

    Enhance changeColor function to include random rejection logic; maintain demo function structure for clarity

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ let h1 = document.querySelector("h1");
  function changeColor(color, delay){
    return new Promise((resolve,reject) => {
          setTimeout(() => {
+            let num = Math.floor(Math.random() * 5);
+            if (num > 3){
+                reject("Promise rejected")
+            }
         h1.style.color = color;
         console.log("Color changed to ",color)
         resolve("color changed")
@@ -28,12 +32,16 @@ let h1 = document.querySelector("h1");
 }
 
 async function demo(){
-    await changeColor("pink",1000);
-    await changeColor("red",1000);
-    await changeColor("green",1000);
-    await changeColor("blue",1000);
-    await changeColor("yellow",1000);
-    await changeColor("orange",1000);
+     await changeColor("pink",1000);
+     await changeColor("red",1000);
+     await changeColor("green",1000);
+     await changeColor("blue",1000);
+     await changeColor("yellow",1000);
+     await changeColor("orange",1000);
+
+     let a = 5;
+     console.log(a);
+
 }
 
 
