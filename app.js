86c6a01b5d7f9f93e1d2dcfbc3399d9dba66871e
mhassan-pro <mhassan.pro0@gmail.com>
2025-09-19 10:11:14 +0500

    Refactor saveToDb function to use Promises for improved asynchronous handling

--- app.js
+++ app.js
@@ -18,33 +18,48 @@
 // });
 
 
-function saveToDb(data, success, failure){
-    let randomInternetSpeed = Math.floor(Math.random() *10) + 1;
-    if(randomInternetSpeed > 5){
-        success();
-    } else{
-       failure();
-    }
-}
+// function saveToDb(data, success, failure){
+//     let randomInternetSpeed = Math.floor(Math.random() *10) + 1;
+//     if(randomInternetSpeed > 5){
+//         success();
+//     } else{
+//        failure();
+//     }
+// }
 
-saveToDb("apna college",() => {
-     console.log("success 1:your data was saved");
-     saveToDb("hello world", 
-        () => {
-            console.log("success 2: data was saved")
-            saveToDb("Hassan",
-                () => {
-                    console.log("success 3: data was saved")
-                },
-                () => {
-                    console.log("failure 3: data was not saved")
-                }
-            )
-        },
-        () => {
-            console.log("failuer 2 : data was not saved")
+// saveToDb("apna college",() => {
+//      console.log("success 1:your data was saved");
+//      saveToDb("hello world", 
+//         () => {
+//             console.log("success 2: data was saved")
+//             saveToDb("Hassan",
+//                 () => {
+//                     console.log("success 3: data was saved")
+//                 },
+//                 () => {
+//                     console.log("failure 3: data was not saved")
+//                 }
+//             )
+//         },
+//         () => {
+//             console.log("failuer 2 : data was not saved")
+//         }
+//      )
+// }, () => {
+//      console.log("weak connection data was not saved")
+// });
+
+function saveToDb(data){
+    return new Promise ( (resolve, reject) => {
+
+        let randomInternetSpeed = Math.floor(Math.random() *10) + 1;
+        if(randomInternetSpeed > 5){
+            resolve("data was saved");
         }
-     )
-}, () => {
-     console.log("weak connection data was not saved")
-});
+        else{
+            reject("data was not saved, weak connection");
+        }
+    });
+}    
+
+saveToDb("Apna COllege");
