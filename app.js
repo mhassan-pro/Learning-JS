c58603efc7f683d970d3f530bf4742cd3e932a83
mhassan-pro <mhassan.pro0@gmail.com>
2025-09-19 22:47:32 +0500

    Update greet function to throw an error for improved error handling

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 let h1 = document.querySelector("h1");
 
 async function greet(){
-    return "hello world"
+    throw "page not found"
+    return "hello world";
+}
+greet()
+.then((result) => {
+    console.log("promise was resolved",result);
+})
+.catch((error) => {
+    console.log("promise was rejected",error)
+})
+
+let demo =async() => {
+    return 5;
 }
