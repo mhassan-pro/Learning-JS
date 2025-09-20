e7eec5c0ce3b60493be2a44560cfc61f49d57172
mhassan-pro <mhassan.pro0@gmail.com>
2025-09-20 19:52:42 +0500

    Add JSON parsing and stringifying examples in app.js and app3.js; enhance clarity with structured student object

--- app.js
+++
@@ -1,5 +0,0 @@
-let jsonRes = `{"fact":"There are approximately 100 breeds of cat.","length":42}`;
-console.log(jsonRes)
-
-let validRes = JSON.parse(jsonRes);
-console.log(validRes);
--- app3.js
+++ app3.js
@@ -474,5 +474,25 @@ async function greet(){
 
  */
 
+    let jsonRes = `{"fact":"There are approximately 100 breeds of cat.","length":42}`;
+console.log(jsonRes)
+
+let validRes = JSON.parse(jsonRes);
+console.log(validRes);
+
+let STUDENT = {
+    name:"Hassan",
+    age: 24,
+    isActive: true
+}
+
+let studentJSON = JSON.stringify(STUDENT);
+console.log(studentJSON)
+
+/* Testing API request
+     ->Hoppscotch
+     ->Postman
+*/
+
 
 
