944f9367e8c8e95a62d80e5cddda1e2ce9b96332
mhassan-pro <mhassan.pro0@gmail.com>
2025-09-21 19:19:50 +0500

    Refactor app.js and app3.js to streamline fetch requests; remove commented code and enhance clarity with async/await and axios

--- app.js
+++ app.js
@@ -1,60 +1,18 @@
-/* First Request using fetch
-this method return promise in foorm of response object
-api calls are working asynchronously
-  */
+
+let btn = document.querySelector("button");
+btn.addEventListener('click', async ()=>{
+    let fact = await getFetch();
+    console.log("fact--",fact);
+})
  let url = "https://catfact.ninja/fact";
-//  fetch(url)
-//  .then((response) => {
-//     console.log("accepted",response);
-//     // response.json().then((data) => {
-//     //     console.log(data); })
-//     return response.json();
-// })
-// .then((data) => {
-//     console.log(data.fact)
-//     console.log(data)
-//     return fetch(url);
-// })
-// .then((response) => {
-//     return response.json();
-// })
-// .then((data) => {
-//     console.log(data.fact)
-//     console.log(data)
-//     return fetch(url)
-// })
-// .then((response) => {
-//     return response.json();
-// })
-// .then((data) => {
-//     console.log(data.fact)
-//     console.log(data)
-// })
-//  .catch((error) => {
-//     console.log("rejected",error)
-//  })
-    /* Second Request using async await
-    */
-async function getFetch(){
-   try{
-    let res = await fetch(url);
-    let data = await res.json();
-    console.log(data)
-    console.log(data.fact)
 
-    let res1 = await fetch(url);
-    let data1 = await res1.json();
-    console.log(data1)
-    console.log(data1.fact)
-
-    let res2 = await fetch(url);
-    let data2 = await res2.json();
-    console.log(data2)
-    console.log(data2.fact)
-}
-catch (err){
-    console.log(err)
-}
-let number  = 123;
-console.log(number)
+ async function getFetch(){
+    try{
+    let req = await axios.get(url);
+    return req.data.fact;
+ }
+ catch(err){
+    console.log("errrr---",err)
+    return "no fact found";
+ }
 }
